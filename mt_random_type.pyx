c688454a768057bfae1b247de227b289e4fa5f6f
Dustin Kerns <Dkerns0123@gmail.com>
2020-03-08 16:02:09 -0700

    Add docstrings

--- mt_random_type.pyx
+++ mt_random_type.pyx
@@ -16,29 +16,36 @@ cdef class MT:
 
 
     cpdef double rand(self):
+        """ Convenience wrapper for rand_real1 """
         return mt_struct.genrand_real1(self._thisptr)
 
 
     cpdef int rand_int(self):
+        """ Generates a random number on interval [0, 0xFFFFFFFF] """
         return mt_struct.genrand_int32(self._thisptr)
 
 
     cpdef long rand_int31(self):
+        """ Generates a random number on interval [0, 0x7FFFFFFF] """
         return mt_struct.genrand_int31(self._thisptr)
 
 
     cpdef double rand_real1(self):
+        """ Generates a random number on real interval [0, 1] """
         return mt_struct.genrand_real1(self._thisptr)
 
 
     cpdef double rand_real2(self):
+        """ Generates a random number on real interval [0, 1) """
         return mt_struct.genrand_real2(self._thisptr)
 
 
     cpdef double rand_real3(self):
+        """ Generates a random number on real interval (0, 1) """
         return mt_struct.genrand_real3(self._thisptr)
 
 
     cpdef double rand_res53(self):
+        """ Generates a random 53-bit number on interval [0, 1) """
         return mt_struct.genrand_res53(self._thisptr)
 
